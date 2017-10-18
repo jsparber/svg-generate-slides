8be51c98586a1e348bde7b90664ede4d85be3b40
Julian Sparber <julian@sparber.net>
2017-10-18 19:08:46 +0200

    [minor] use only rect objects within a group

--- generate-slides.sh
+++ generate-slides.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 #Usage
-#./generate-slides.sh "#color" input.svg output.pdf
+#./generate-slides.sh "#color" slideGroupId input.svg output.pdf
 # Used apps Xcfb, inkscape, stapler
 
 if [ -z "$1" ] ; then 
   if [ -z "$2" ] ; then 
     if [ -z "$3" ] ; then 
-      echo 'Usage: ./generate-slides.sh "#color" input.svg output.pdf'
+      echo 'Usage: ./generate-slides.sh "#color" slideGroupId input.svg output.pdf'
       echo 'Used apps: Xcfb, inkscape, stapler, grep (make sure to have them installed)'
 
     fi
   fi
 else
   COLOR_INPUT=$1
-  INPUT=$2
-  OUTPUT=$3
+  GROUP=$2
+  INPUT=$3
+  OUTPUT=$4
   echo Export all slides with background color $1 in file $2
   Xvfb :100 &
   OLDDISPLAY=$DISPLAY
@@ -24,10 +25,12 @@ else
   COLOR="fill:${COLOR_INPUT}"
 
   TMP="/tmp/slide_exporter_$RANDOM"
-
-  objList=`inkscape --file $INPUT --query-all | grep rect | sort -n -t "," -k 3,3 | cut -d "," -f 1`
-
   mkdir $TMP
+
+  `inkscape -z --export-id=${GROUP} --export-plain-svg=$TMP/group.svg --export-id-only -f $INPUT`
+
+  objList=`inkscape --file $TMP/group.svg --query-all | grep rect | sort -n -t "," -k 3,3 | cut -d "," -f 1`
+
   i=0
   for ID in $objList; do
     `inkscape -z --export-id=${ID} --export-plain-svg=$TMP/${ID}.svg --export-id-only -f $INPUT`
